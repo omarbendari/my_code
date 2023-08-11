32a134448bdc34bb5517a28a1ef17a02c57337bd
omarbendari <141953654+omarbendari@users.noreply.github.com>
2023-08-11 17:04:55 +0300

    Add files via upload
    A program that determines the largest number out of 3 numbers

---
+++ -1 python (بيطلع اكبر رقم ضمن ثلاث ارقام).py
@@ -0,0 +1,16 @@
+# سؤال4 (ابحث عن اكبر رقم ضمن ثلاث ارقام)
+x=1
+y=2
+z=3
+if x>y>z:
+ print(x)
+if z>y>x:
+ print(z)
+if y>x>z:
+ print(y)
+if x>z>y:
+ print(x)
+if z>x>y: 
+ print(z)
+if y>z>x:
+ print(y)
