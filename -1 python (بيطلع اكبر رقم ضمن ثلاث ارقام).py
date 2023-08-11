ee32ae6e500047823750e8d8ce329faf135ba537
omarbendari <141953654+omarbendari@users.noreply.github.com>
2023-08-11 17:06:10 +0300

    Update -1 python (بيطلع اكبر رقم ضمن ثلاث ارقام).py
    A program that determines the largest number out of 3 numbers

--- -1 python (بيطلع اكبر رقم ضمن ثلاث ارقام).py
+++ -1 python (بيطلع اكبر رقم ضمن ثلاث ارقام).py
@@ -4,13 +4,13 @@ y=2
 z=3
 if x>y>z:
  print(x)
-if z>y>x:
+elif z>y>x:
  print(z)
-if y>x>z:
+elif y>x>z:
  print(y)
-if x>z>y:
+elif x>z>y:
  print(x)
-if z>x>y: 
+elif z>x>y: 
  print(z)
-if y>z>x:
+elif y>z>x:
  print(y)
